710fa202465d8cf307d87ce136415c8558c4dddc
Kieran <kags1995@hotmail.co.uk>
2015-05-07 02:10:04 +0100

    New 2D Movement
    to see this feature simply play(please don't save edits though) Kierans
    test scene, stamina bar as well as deadzone has also been added. left
    click to move normally, right click for boost.

---
+++ UnityProjectNoPills/Assets/Scripts/PlayerCharacter_CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter_CameraFollow : MonoBehaviour {
+    
+    public Transform targetObject;
+    private Vector3 targetDestination;
+    public float dampTime = 0.3f;
+
+    void Start()
+    {
+        targetDestination = new Vector3(0, 0, transform.position.z);
+    }
+
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        targetDestination.x = Mathf.Lerp(transform.position.x, targetObject.position.x, dampTime * Time.deltaTime);
+        targetDestination.y = Mathf.Lerp(transform.position.y, targetObject.position.y, dampTime * Time.deltaTime);
+        transform.position = targetDestination;
+    }
+}
---
+++ UnityProjectNoPills/Assets/Scripts/PlayerCharacter_GraphicRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter_GraphicRotation : MonoBehaviour {
+
+    public Transform parentObject;
+    private Quaternion originalRotation;
+
+	// Use this for initialization
+	void Start () {
+        originalRotation = transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (parentObject.rotation.eulerAngles.z > 90 && parentObject.rotation.eulerAngles.z < 270)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.rotation, new Quaternion(180,0,0,0),1);
+        }
+        if (parentObject.rotation.eulerAngles.z < 90 || parentObject.rotation.eulerAngles.z > 270)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.rotation, originalRotation, 1); 
+        }
+	}
+}
