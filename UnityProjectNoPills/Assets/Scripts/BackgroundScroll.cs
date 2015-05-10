499a7df16d69d305ae2cc287d58d9fadf5c2b529
Kieran <kags1995@hotmail.co.uk>
2015-05-10 13:53:44 +0100

    Camera & Backgrounds scroll tests
    Camera now has top and bottom limits, background scrolling was also
    worked on but tests proved to be lackluster so scrolling the background
    has been abandoned for now

---
+++ UnityProjectNoPills/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour {
+
+    public float characterSpeed;
+	public GameObject characterObject;
+	public float backgroundSpeed;
+	public float textureOffSetX;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 xPos = transform.position;
+		xPos.x = characterObject.transform.position.x;
+		transform.position = xPos;
+
+		textureOffSetX = GetComponent<Renderer>().material.mainTextureOffset.x;
+
+		characterSpeed = characterObject.GetComponent<Rigidbody2D>().velocity.x;
+        backgroundSpeed += (-characterSpeed / (characterObject.GetComponent<PlayerCharacter_Movement>().maxSpeed*2)) * Time.deltaTime;
+
+        if (textureOffSetX >= 1 || textureOffSetX <= -1)
+        {
+            textureOffSetX = 0;
+        }
+        if (characterSpeed < 0.1 && characterSpeed > -0.1)
+        {
+            backgroundSpeed = 0;
+        }
+        if (characterSpeed > 0.1 || characterSpeed < -0.1)
+        {
+            GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2((textureOffSetX + backgroundSpeed), 0));
+        }
+	}
+}
--- UnityProjectNoPills/Assets/Scripts/PlayerCharacter_CameraFollow.cs
+++ UnityProjectNoPills/Assets/Scripts/PlayerCharacter_CameraFollow.cs
@@ -4,50 +4,34 @@ using System.Collections;
 public class PlayerCharacter_CameraFollow : MonoBehaviour {
     
     public Transform targetObject;
+    public float topLimit;
+    public float botLimit;
     private Vector3 targetDestination;
-    /*public float deadzone = 0.5f;
-    public float moveSpeed = 0.7f;
-    public float deltaX = 0;
-    public float deltaY = 0;*/
-
+    bool canMoveY = true;
 
     void Start()
     {
         targetDestination = transform.position;
     }
 
-
-
     // Update is called once per frame
+    float timer;
     void LateUpdate()
     {
-        /*if (targetObject.position.x > targetDestination.x)
+        if (targetObject.position.y < topLimit && targetObject.position.y > botLimit)
         {
-            deltaX = targetObject.position.x - transform.position.x;
+            targetDestination.y = targetObject.position.y;
         }
-        if (targetObject.position.x < targetDestination.x)
-        {
-            deltaX = transform.position.x - targetObject.position.x;
-        }
-        if (deltaX > deadzone)
-        {
-            targetDestination.x = Mathf.Lerp(targetDestination.x, targetObject.position.x, moveSpeed * Time.deltaTime);
-        }
-        if (targetObject.position.y > targetDestination.y)
-        {
-            deltaY = targetObject.position.y - transform.position.y;
-        }
-        if (targetObject.position.y < targetDestination.y)
-        {
-            deltaY = transform.position.y - targetObject.position.y;
-        }
-        if (deltaY > deadzone)
-        {
-            targetDestination.y = Mathf.Lerp(targetDestination.y, targetObject.position.y, moveSpeed*Time.deltaTime);
-        }*/
-
         targetDestination.x = targetObject.position.x;
-        targetDestination.y = targetObject.position.y;
         transform.position = targetDestination;
     }
+
+   /* void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Level Edges")
+        {
+            Debug.Log("Can't move");
+            canMove = false;
+        }
+    }*/
 }
